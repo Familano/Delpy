de30848d5cf3d14328b50358c335ac21c832f3e2
Setyawanj65 <setyawanj65@gmail.com>
2018-10-09 22:57:31 +0700

    Update standarisasi ID element di Login Pasien dan mengurutkan xml di login pasien dan dokter

--- app/src/main/java/com/example/maming/delpy/ActLoginPasien.java
+++ app/src/main/java/com/example/maming/delpy/ActLoginPasien.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ActLoginPasien extends AppCompatActivity {
@@ -13,32 +14,42 @@ public class ActLoginPasien extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_loginpasien);
-        final Button pasienButton = (Button) findViewById(R.id.btnPasien);
-        final Button dokterButton = (Button) findViewById(R.id.btnDokter);
-        final Button loginButton = (Button) findViewById(R.id.btnLogin);
-        final TextView lupaPass = (TextView)findViewById(R.id.txtLupaPassword);
-        final Button daftarButton = (Button) findViewById(R.id.btnDaftar);
+        final Button pasienButton = (Button) findViewById(R.id.ALPas_BtnPasien);
+        final Button dokterButton = (Button) findViewById(R.id.ALPas_BtnDokter);
+        final EditText emailLogin = (EditText) findViewById(R.id.ALPas_TxfEmail);
+        final EditText passLogin = (EditText)findViewById(R.id.ALPas_TxfPassword);
+        final Button loginButton = (Button) findViewById(R.id.ALPas_BtnLogin);
+        final TextView lupaPass = (TextView)findViewById(R.id.ALPas_TxtLupaPassword);
+        final Button daftarButton = (Button) findViewById(R.id.ALPas_BtnDaftar);
+
+        // Function for button Pasien when onClick
         pasienButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent(ActLoginPasien.this, ActLoginPasien.class);
                 startActivity(intent);
             }
         });
+
+        // Function for button Dokter when onClick
         dokterButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent(ActLoginPasien.this, ActLoginDokter.class);
                 startActivity(intent);
             }
         });
+
+        // Function for button Login when onClick
         loginButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-               /* String emaillogin = String.valueOf(findViewById(R.id.txfEmail));
-                String passlogin = String.valueOf(findViewById(R.id.txfPassword));
+               /* String emaillogin = String.valueOf(emailLogin.getText());
+                String passlogin = String.valueOf(passLogin.getText());
                 lupaPass.setText("email " + emaillogin + " , password " + passlogin);*/
                 Intent intent = new Intent(ActLoginPasien.this, ActQuestion1.class);
                 startActivity(intent);
             }
         });
+
+        // Function for button Daftar when onClick
         daftarButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent(ActLoginPasien.this, ActRegisterPasien.class);
