70158d7b1195ec466bce21d9aaa78bea7a630663
yasminnurfatikah <yasminftkh08@gmail.com>
2018-11-02 12:03:33 +0700

    backup

---
+++ app/src/main/java/com/example/maming/delpy/ActTreatmentPasien.java
@@ -0,0 +1,11 @@
+package com.example.maming.delpy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActTreatmentPasien extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_homepasien);
+    }
+}
